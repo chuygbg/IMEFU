9b057cf02f7f820da6f99316a7751811db2a4512
unknown <jesusgg.b1@gmail.com>
2021-08-08 19:29:16 -0500

    Version Alpha

--- torneo_de_debate/src/App.js
+++ torneo_de_debate/src/App.js
@@ -1,12 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from "./components/header"
-import "bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
-  return (
-    <Header></Header>
-  );
+import './App.css';
+import "./index.css"
+//Importing components
+import Header from "./components/header"
+import Main from "./components/main"
+import Footer from "./components/footer"
+import Landing from "./components/landing"
+
+//End of imports
+import "bootstrap/dist/css/bootstrap.min.css"
+import React from 'react';
+
+function App(User) {
+  if(User){
+    return (
+      <React.Fragment>
+        <Header></Header>
+        <Main>
+        </Main>
+        <Footer></Footer>
+      </React.Fragment>
+      );
+  }else{
+    return(
+    <React.Fragment>
+      <Header></Header>
+      <Landing></Landing>
+      <Footer></Footer>
+    </React.Fragment>
+    )
+  }
+
+
+
+  
 }
 
 export default App;
--- torneo_de_debate/src/components/header.js
+++ torneo_de_debate/src/components/header.js
@@ -1,33 +1,41 @@
 import React from "react"
+import logo from "../images/LogoBlanco.png"
 
 function Header(){
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <React.Fragment>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <img src={logo} style={{width:"10em", margin:"0.5em"}}/>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        <a className="nav-link active" aria-current="page" href="#">Inicio</a>
                         </li>
                         <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
+                        <a className="nav-link active" onClick={Login()}>Login</a>
                         </li>
-                        <li className="nav-item">
-                        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                        </li>
+
                     </ul>
-                    <form className="d-flex">
-                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
+
                 </div>
             </div>
         </nav>
+        <div id="here"></div>
+
+        </React.Fragment>
     )
+
+
+
 }
+
+function Login(){
+   document.createElement("div").innerHTML="<h1>Works</h1>"
+}
+
 
 export default Header
